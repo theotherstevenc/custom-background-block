804ff2a1f2ab62017d33e87a934bfa940fcc233f
theotherstevenc <steven.winston.collins@gmail.com>
2025-06-26 10:54:59 -0700

    chore: move SDK declaration from blocksdk.d.ts to vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+declare module 'blocksdk' {
+  class SDK {
+    constructor(arg1?: unknown, arg2?: unknown, arg3?: boolean)
+    setData(key: string, value: string): void
+    getData(key: string): string | undefined
+    setContent(content: string): void
+  }
+
+  export default SDK
+}
