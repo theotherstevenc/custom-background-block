78661a502db150dd5f09c1c84fcf15a1df486ab0
theotherstevenc <steven.winston.collins@gmail.com>
2025-06-26 15:25:52 -0700

    refactor: simplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,38 @@
 import { Box } from '@mui/material'
-import Autocomplete from '@mui/material/Autocomplete'
 import TextField from '@mui/material/TextField'
 import { useEffect, useState } from 'react'
 import SDK from 'blocksdk'
 
-interface GoogleFontsResponse {
-  fonts: string[]
-  count: number
-  limit: number
-  sortBy: string
-}
-
 const sdk = new SDK(null, null, true)
 
 function App() {
-  const [imageURL, setImageURL] = useState<string>(sdk.getData('imageURL') || 'https://placecats.com/millie/500/500')
-  const [imageWidth, setImageWidth] = useState<string>(sdk.getData('imageWidth') || '500')
+  const [imageURL, setImageURL] = useState<string>(sdk.getData('imageURL') || 'https://placecats.com/millie/420/500')
+  const [imageWidth, setImageWidth] = useState<string>(sdk.getData('imageWidth') || '420')
   const [imageHeight, setImageHeight] = useState<string>(sdk.getData('imageHeight') || '500')
+  const [imageBackgroundColor, setImageBackgroundColor] = useState<string>(
+    sdk.getData('imageBackgroundColor') || '#808080'
+  )
   const [textOverlay, setTextOverlay] = useState<string>(sdk.getData('textOverlay') || 'scratch or die')
-  const [textColor, setTextColor] = useState<string>(sdk.getData('textColor') || 'orange')
-  const [textBackgroundColor, setTextBackgroundColor] = useState<string>(sdk.getData('textBackgroundColor') || 'black')
-  const [googleFonts, setGoogleFonts] = useState<GoogleFontsResponse | null>(null)
+  const [textColor, setTextColor] = useState<string>(sdk.getData('textColor') || '#FFA500')
+  const [textBackgroundColor, setTextBackgroundColor] = useState<string>(
+    sdk.getData('textBackgroundColor') || '#000000'
+  )
 
-  const handleChange = (_event: React.SyntheticEvent, value: string[]) => {
-    console.log('Selected Google Fonts:', value)
-  }
-
-  const API_URL = 'http://localhost:5173/api/fonts'
-  const HTTP_METHOD_POST = 'GET'
-
-  const getGoogleFonts = async () => {
-    try {
-      const response = await fetch(API_URL, {
-        method: HTTP_METHOD_POST,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`)
-      }
-
-      const data = await response.json()
-      setGoogleFonts(data)
-    } catch (error) {
-      console.error('Error fetching Google Fonts:', error)
-    }
-  }
-
-  useEffect(() => {
-    getGoogleFonts()
-  }, [])
+  const vmlWidth = Number(imageWidth) * 0.75
+  const vmlHeight = Number(imageHeight) * 0.75
 
   useEffect(() => {
     const markUp = `
-    <table role="none" style="width:${imageWidth}px;" cellpadding="0" cellspacing="0" border="0" class="w100pc">
+    <table role="none" style="width:${imageWidth}px;" cellpadding="0" cellspacing="0" border="0" class="outer-tbl">
       <tr>
-        <td align="center" bgcolor="${textBackgroundColor}" background="${imageURL}" width="${imageWidth}" height="${imageHeight}" valign="middle" style="color: ${textColor}; background: url(${imageURL}) center / cover no-repeat ${textBackgroundColor}; background-position: center; background-size: cover; background-repeat: no-repeat;" class="w100pc">
+        <td align="center" bgcolor="${imageBackgroundColor}" background="${imageURL}" width="${imageWidth}" height="${imageHeight}" valign="middle" style="color: ${textColor}; background: url(${imageURL}) center / cover no-repeat ${imageBackgroundColor}; background-position: center; background-size: cover; background-repeat: no-repeat;" class="w100pc">
           <!--[if gte mso 9]>
-          <v:image xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style=" border: 0;display: inline-block; width: ${
-            Number(imageWidth) * 0.75
-          }pt; height: ${Number(imageHeight) * 0.75}pt;" src="${imageURL}" />
-          <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="f" stroke="false" style="border: 0;display: inline-block;position: absolute; width: ${
-            Number(imageWidth) * 0.75
-          }pt; height:${Number(imageHeight) * 0.75}pt;">
-            <v:fill opacity="0%" color="${textBackgroundColor}" />
+          <v:image xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style=" border: 0;display: inline-block; width: ${vmlWidth}pt; height: ${vmlHeight}pt;" src="${imageURL}" />
+          <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="f" stroke="false" style="border: 0;display: inline-block;position: absolute; width: ${vmlWidth}pt; height:${vmlHeight}pt;">
+            <v:fill opacity="0%" color="${imageBackgroundColor}" />
             <v:textbox inset="0,0,0,0">
           <![endif]-->
-                  <table role="none" style="width:${imageWidth}px;" cellpadding="0" cellspacing="0" border="0" class="w100pc">
+                  <table role="none" style="width:${imageWidth}px;" cellpadding="0" cellspacing="0" border="0" class="iner-tbl">
                     <tr>
                       <td align="center" bgcolor="${textBackgroundColor}">${textOverlay}</td>
                     </tr>
@@ -81,9 +45,18 @@ function App() {
       </tr>
     </table>
 `
-    console.log('MarkUp:', markUp)
     sdk.setContent(markUp)
-  }, [imageURL, imageWidth, imageHeight, textOverlay, textColor, textBackgroundColor])
+  }, [
+    imageBackgroundColor,
+    imageURL,
+    imageWidth,
+    imageHeight,
+    textOverlay,
+    textColor,
+    textBackgroundColor,
+    vmlWidth,
+    vmlHeight,
+  ])
 
   const handleImageURL = (event: React.ChangeEvent<HTMLInputElement>) => {
     setImageURL(event.target.value)
@@ -113,6 +86,11 @@ function App() {
   const handleTextBackgroundColor = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTextBackgroundColor(event.target.value)
     sdk.setData('textBackgroundColor', event.target.value)
+  }
+
+  const handleImageBackgroundColor = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setImageBackgroundColor(event.target.value)
+    sdk.setData('imageBackgroundColor', event.target.value)
   }
 
   return (
@@ -154,17 +132,32 @@ function App() {
           onChange={handleTextOverlay}
           slotProps={{ inputLabel: { shrink: true } }}
         />
-        <TextField id='outlined-basic' label='Text Color' variant='outlined' type='color' fullWidth onChange={handleTextColor} />
-        <TextField id='outlined-basic' label='Text Background Color' variant='outlined' type='color' fullWidth onChange={handleTextBackgroundColor} />
-
-        <Autocomplete
-          multiple
-          fullWidth={true}
-          id='multiple-limit-tags'
-          options={googleFonts?.fonts || []}
-          getOptionLabel={(fontName) => fontName}
-          renderInput={(params) => <TextField {...params} label='Select Google Fonts' placeholder='Favorites' />}
-          onChange={handleChange}
+        <TextField
+          id='outlined-basic'
+          label='Text Color'
+          variant='outlined'
+          type='color'
+          fullWidth
+          value={textColor}
+          onChange={handleTextColor}
+        />
+        <TextField
+          id='outlined-basic'
+          label='Text Background Color'
+          variant='outlined'
+          type='color'
+          fullWidth
+          value={textBackgroundColor}
+          onChange={handleTextBackgroundColor}
+        />
+        <TextField
+          id='outlined-basic'
+          label='Image Background Color'
+          variant='outlined'
+          type='color'
+          fullWidth
+          value={imageBackgroundColor}
+          onChange={handleImageBackgroundColor}
         />
       </Box>
     </>
